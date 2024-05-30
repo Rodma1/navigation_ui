5b2fc817f5f37d9df22c863b56a5843fa1614664
“Rodma1” <1156028692@qq.com>
2024-05-30 19:38:14 +0800

    feat: 导航栏

---
+++ src/views/NavMenuView.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id = "navMenu">
+        <el-container>
+
+            <el-header>
+                <el-menu :default-active="activeIndex" :router='true' class="el-menu-demo" mode="horizontal"
+                    @select="handleSelect">
+                    <el-menu-item v-for="(item, index) in itemList" :index="item.path" :key="index">
+                        <span slot="title">{{ item.title }}</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-header>
+            <el-main>
+                <!-- 在模板中使用 <router-view> 来显示组件 -->
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "navMenu",
+    data() {
+        return {
+            activeIndex: 'home',
+            itemList: [
+                { path: '/home', title: '首页' },
+                { path: '/about', title: '关于我' },
+                { path: '/menu', title: '收集的网站'}
+            ]
+        };
+    },
+    watch: {
+        $route() {
+            this.setCurrentRoute()
+        }
+    },
+    // computed: {
+    //     toIndex() {  // 根据路径绑定到对应的一级菜单，防止页面刷新重新跳回第一个
+    //         return '/' + this.$route.path.split('/')[1];
+    //     },
+    // },
+    mounted() {
+    },
+    created() {
+        this.setCurrentRoute()
+    },
+    methods: {
+        handleSelect(path) {
+            console.log(path);
+            this.activeIndex = path
+        },
+        setCurrentRoute() {
+            this.activeIndex = this.$route.name;//关键   通过他就可以监听到当前路由状态并激活当前菜单
+        }
+
+    }
+}
+</script>
+
+<style>
+.el-menu-demo {
+    background-color:antiquewhite !important;
+}
+
+
+</style>
+
+<!-- 参考：https://blog.csdn.net/qq_48961214/article/details/130345936 -->
